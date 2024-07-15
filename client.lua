927864ecdcbdc15df527852b7a993bdc37b45ebf
Sarbatore <valentin.chene.p@gmail.com>
2024-07-15 14:50:08 +0200

    Init

---
+++ client.lua
@@ -0,0 +1,61 @@
+local play = false
+
+--- Return whether the player is near the given coordinates
+---@param x number
+---@param y number
+---@param z number
+---@return boolean
+local function IsPlayerNearCoords(x, y, z)
+    local px, py, pz = table.unpack(GetEntityCoords(PlayerPedId(), 0))
+    local distance = GetDistanceBetweenCoords(px, py, pz, x, y, z, false)
+
+    if distance <= 1 then
+        return true
+    end
+end
+
+--- Return whether the player is near the given coordinates
+---@return vector4
+local function NearPiano()
+    for _, coords in ipairs(Config.pianos) do
+        if (IsPlayerNearCoords(coords)) then
+            return coords
+        end
+    end
+    return nil
+end
+
+Citizen.CreateThread(function()
+    local promptGroup = UipromptGroup:new(Config.language.piano)
+    local prompt = Uiprompt:new(0x760A9C6F, Config.language.press, promptGroup)
+        :setOnControlJustPressed(function(_, location)
+            play = not play
+
+            if (play) then
+                local scenario = IsPedMale(PlayerPedId()) and "PROP_HUMAN_PIANO" or "PROP_HUMAN_ABIGAIL_PIANO"
+                TaskStartScenarioAtPosition(PlayerPedId(), GetHashKey(scenario), location, 0, true, true, 0, true)
+
+            else
+                ClearPedTasks(PlayerPedId())
+            end
+        end)
+
+    local sleep = true
+	while true do
+        if (sleep) then
+            Citizen.Wait(1000)
+        else
+            Citizen.Wait(5)
+        end
+        
+        local piano = NearPiano()
+        if (piano) then
+            promptGroup:handleEvents(piano)
+            promptGroup:setActiveThisFrame()
+
+            sleep = false
+        else
+            sleep = true
+        end
+    end    
+end)
---
+++ config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.pianos = {
+    vector4(-312.3, 799.01, 118.46, 102.37), -- Valentine
+    vector4(2631.946 - 0.22, -1232.617 + 0.3, 54.19 - 0.46, 67.788), -- St Denis
+    vector4(2799.582 + 1.0, -1163.929 - 0.3, 47.92 - 0.40, 237.40), -- ST Denis  doyels
+    vector4(-814.99 - 0.03, -1313.581 + 0.5, 43.68 - 0.53, 359.25), -- Blackwater
+    vector4(-5515.401 - 0.5,  -2914.091 - 0.5 , -1.75 - 0.43, 114.87), -- Tumbleweed
+    vector4(1346.77, -1371.1, 80.5, 343.62), -- Rhodes
+}
+
+Config.language = {
+    press = "Press",
+    piano = "Piano",
+}
