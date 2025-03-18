339f5600ad70f4121489773730284a9dc32874d1
Sarbatore <66779630+Sarbatore@users.noreply.github.com>
2025-03-18 09:18:14 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,15 +1,12 @@
-games { "rdr3" }
+game "rdr3"
 fx_version "adamant"
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 lua54 "yes"
+
 author "Sarbatore"
+description "Piano script for RedM"
 
 client_scripts {
-	"@uiprompt/uiprompt.lua",
-	"client.lua"
-}
-
-escrow_ignore {
-	"client.lua",
 	"config.lua",
+	"client.lua"
 }
